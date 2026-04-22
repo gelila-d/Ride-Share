57b457fb6bc0b7f0d2bd4ba415e760eb9342cae3
gelila-d <gelilad1111@gmail.com>
2026-04-22 19:10:02 +0100

    feat: add LoginController and notification for login verification; update composer dependencies

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function submit(Request $request){
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+        ]);
+
+        $user = User:: firstOrCreate([
+            'phone' => $request->phone,
+        ]);
+
+        if(!$user){
+            return response()->json([
+                'message' => 'Could not process a user with that phone number',
+            ], 401);
+        }
+
+        $user->notify();
+    }
+}
