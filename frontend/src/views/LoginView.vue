ae2b751baa673a68398285b675949103513a2450
gelila-d <gelilad1111@gmail.com>
2026-04-28 06:43:23 +0100

    feat: implement phone number authentication and verification flow in LoginView

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { reactive} from 'vue';
+import { reactive, ref} from 'vue';
 import axios from 'axios';
 
 const credentials = reactive({
-  phone: null
+  phone: null,
+  login_code: null
 });
+
+const waitingOnVerification = ref(false);
 const handleLogin =() =>{
   const formattedPhone = credentials.phone.replace(/[^0-9]/g, '').replace(/^251/, '0');
   axios.post('http://127.0.0.1:8000/api/login', {
@@ -12,6 +15,7 @@ const handleLogin =() =>{
   })
   .then(response => {
     console.log(response.data);
+    waitingOnVerification.value = true
   })
   .catch(error => {
   if (error.response) {
@@ -22,14 +26,35 @@ const handleLogin =() =>{
 })
 }
 
+const handleVerification =() =>{
+  axios.post('http://127.0.0.1:8000/api/verify', {
+    phone: credentials.phone.replace(/[^0-9]/g, '').replace(/^251/, '0'),
+    login_code: credentials.login_code
+  })
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    if (error.response) {
+      console.log(error.response.data);
+    } else {
+      console.log(error.message); // network error
+    }
+  });
+
+
+}
+
 </script>
 
 <template>
   <div class="min-h-screen flex items-center justify-center">
     <div class="w-full max-w-sm">
-      <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
+      <h1 class="text-3xl font-semibold mb-4">
+        {{ waitingOnVerification ? 'Enter your verification code' : 'Enter your phone number' }}
+      </h1>
 
-      <form action="#" @submit.prevent="handleLogin">
+      <form v-if="!waitingOnVerification" @submit.prevent="handleLogin">
         <div class="overflow-hidden shadow sm:rounded-md text-left">
           <div class="bg-white px-4 py-5 sm:p-6">
             <input
@@ -38,17 +63,44 @@ const handleLogin =() =>{
               id="phone"
               placeholder="+251 91 234 5678"
               class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm"
-              v-maska data-maska="+251 ## ### ####"
+              v-maska
+              data-maska="+251 ## ### ####"
               v-model="credentials.phone"
             />
           </div>
 
           <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-            <button @submit="handleLogin"
+            <button
               type="submit"
               class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-white"
             >
               Continue
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <form v-else @submit.prevent="handleVerification">
+        <div class="overflow-hidden shadow sm:rounded-md text-left">
+          <div class="bg-white px-4 py-5 sm:p-6">
+            <input
+              type="text"
+              name="code"
+              id="code"
+              placeholder="Enter 6-digit code"
+              class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm"
+              v-maska
+              data-maska="######"
+              v-model="credentials.login_code"
+            />
+          </div>
+
+          <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <button
+              type="submit"
+              class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-white"
+            >
+              Verify
             </button>
           </div>
         </div>
