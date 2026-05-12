817a3be8fb4b2ba91c8992c96e6ad758af2e8fa3
gelila-d <gelilad1111@gmail.com>
2026-05-12 05:59:37 +0100

    feat: add StandbyView component with an animated loading indicator

--- frontend/src/views/StandbyView.vue
+++ frontend/src/views/StandbyView.vue
@@ -1,9 +1,15 @@
 <script setup>
+import Loader from '@/components/Loader.vue'
+
 
 </script>
 
 <template>
     <div class="pt-16">
-        <h1 class="text-3xl font-semibold mb-4">You're all set</h1>
+        <h1 class="text-3xl font-semibold mb-4">Waiting for ride request...</h1>
+        <div class=" mt-8 flex justify-center">
+            <Loader />
+        </div>
+
     </div>
 </template>
